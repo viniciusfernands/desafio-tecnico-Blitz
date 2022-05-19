dc7e18e67fb3273f65c398be693631d76c7ee126
viniciusfernands <viniciusfernandes126@gmail.com>
2022-05-19 17:46:18 -0300

    feat: service method to create new task

---
+++ backend/src/services/tasks.services.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+import { INewTask } from '../interfaces/tasks.interfaces';
+import prisma from '../server';
+
+class TasksServices {
+  constructor(private server: PrismaClient = prisma) {}
+
+  public create = async (newTask: INewTask) => {
+    try {
+      const created = await this.server.tasks.create({ data: { ...newTask } });
+      return created;
+    } catch (error) {
+      return false;
+    }
+  };
+}
+
+const services = new TasksServices();
+export default services;

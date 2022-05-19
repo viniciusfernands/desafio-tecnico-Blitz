b6163a70b6a07ed0a1cd10c053186200f0176016
viniciusfernands <viniciusfernandes126@gmail.com>
2022-05-19 17:49:38 -0300

    feat: method responsible for returning the bank information

---
+++ backend/src/controllers/tasks.controllers.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import TasksServices from '../services/tasks.services';
+
+const ERROR_MESSAGE = { message: 'Algo deu errado' };
+
+class TasksControllers {
+  constructor(private service = TasksServices) {}
+
+  public create = async (req: Request, res: Response) => {
+    try {
+      const { task, status } = req.body;
+
+      const created = await this.service.create({ task, status });
+
+      if (!created) return res.status(400).json({ message: 'Não foi possivel criar nova tarefa' });
+
+      return res.status(201).json(created);
+    } catch (error) {
+      return res.status(500).json(ERROR_MESSAGE);
+    }
+  };
+}
+
+const controllers = new TasksControllers();
+export default controllers;
